9fc3d9f0e78e5b328431cce550acce23c618003f
Liam Sidney <liamesidney@gmail.com>
2023-11-24 15:17:14 +0000

    Commit Module 3 Challenge

---
+++ 03-Terminal-and-Bash/Challenge/BlackJack-dealers.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Define the date and time list
+dates_and_times=(
+  "0310 05:00:00 AM"
+  "0310 08:00:00 AM"
+  "0310 02:00:00 PM"
+  "0310 08:00:00 PM"
+  "0310 11:00:00 PM"
+  "0312 05:00:00 AM"
+  "0312 08:00:00 AM"
+  "0312 02:00:00 PM"
+  "0312 08:00:00 PM"
+  "0312 11:00:00 PM"
+  "0315 05:00:00 AM"
+  "0315 08:00:00 AM"
+  "0315 02:00:00 PM"
+)
+
+# Loop through each date and time
+for dt in "${dates_and_times[@]}"; do
+  # Extract date and time components
+  date=$(echo "$dt" | awk '{print $1}')
+  time=$(echo "$dt" | awk '{print $2}')
+  time_M=$(echo "$dt" | awk '{print $3}')
+
+  # Extract black jack dealer information using awk
+  dealer_info=$(awk -v time="$time" -v time_M="$time_M" '$1 == time && $2 == time_M {print $3, $4}' "${date}_Dealer_schedule.txt")
+
+  # Print the result
+  echo "Loss occurred on $date at $time $time_M. Black Jack Dealer information: $dealer_info"
+done
---
+++ 03-Terminal-and-Bash/Challenge/dealer_finder_by_time.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+ 
+# Check if three arguments are provided
+if [ "$#" -ne 3 ]; then
+	echo "Usage: $0 <date (MMYY)> <Time> <AM or PM>"
+    exit 1
+fi
+
+# Set common name variable to arguments 
+date="$1"
+time="$2"
+time_period="$3"
+
+# Check if Dealer Schedule file exists
+if [ ! -f "${date}_Dealer_schedule.txt" ]; then
+	echo "Error: Dealer Schedule file ${date}_Dealer_schedule.txt not found."
+	echo "Resolution: Ensure Dealer Schedule ${date}_Dealer_schedule.txt is in the same directory as $0 script."
+	exit 1
+fi
+
+# Function to search all files in directory for losses during provided time
+search_losses() {
+	# Set Local Variables
+	local date="$1"
+	local time="$2"
+	local time_period="$3"
+	
+	# Dealers Function takes in two time variables to pass out dealers were dealing at that time
+	local dealers=$(awk -v time="$time" -v time_period="$time_period" '$1 == time && $2 == time_period {print $3, $4, $5, $6, $7, $8}' "${date}_Dealer_schedule.txt")
+	search_losses=($dealers)
+}
+
+# Call the function
+search_losses "$date" "$time" "$time_period"
+
+# Echo results from function
+echo "The Black Jack Dealer was ${search_losses[0]} ${search_losses[1]}."
+echo "The Roulette Dealer was ${search_losses[2]} ${search_losses[3]}."	
+echo "The Texas Hold EM Dealer was ${search_losses[4]} ${search_losses[5]}."
